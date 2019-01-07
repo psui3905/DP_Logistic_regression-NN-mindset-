ab42deccf4d5f4137021d1a3524b5fb8f8dea094
Pengwei Sui <psui3905@uni.sydney.edu.au>
2019-01-07 21:17:10 +1100

    method update except load_dataset

--- cat_logistic_regression.py
+++ cat_logistic_regression.py
@@ -1,9 +1,9 @@
 import numpy as np
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 # import h5py
-# import scipy
+import scipy
 # from PIL import Image
-# from scipy import ndimage
+from scipy import ndimage
 # from lr_utils import load_dataset
 
 # Overview of the Problem set
@@ -121,6 +121,9 @@ def propagate(w, b, X, Y):
     # dw.shape should be (num_px * nump_px * 3, 1), db should be a scalar
     assert(dw.shape == w.shape)
     assert(db.dtype == float)
+
+    # np.squeeze(arr) -- remove one dimentional entry from the shape of given array. 
+    #   -- arr: input array
     cost = np.squeeze(cost)
     assert(cost.shape == ())
     
@@ -187,5 +190,109 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = False):
 
 
 
+def predict(w, b, X):
+    '''
+    Predict whether the label is 0 or 1 using learned logistic regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
+    b -- bias, a scalar
+    X -- data of size (num_px * num_px * 3, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for the examples in X
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1,m))
+    w = w.reshape(X.shape[0], 1)
+    
+    # Compute vector "A" predicting the probabilities of a cat being present in the picture
+    A = sigmoid(np.dot(w.T, X) + b)
+     
+    for i in range(A.shape[1]):
+        
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        if(A[0][i] > 0.5):
+            Y_prediction[0][i] = 1;
+            
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
 
 
+# Merge all functions into a model
+def model(X_train, Y_train, X_test, Y_test, num_iterations = 2000, learning_rate = 0.5, print_cost = False):
+    """
+    Builds the logistic regression model by calling the function you've implemented previously
+    
+    Arguments:
+    X_train -- training set represented by a numpy array of shape (num_px * num_px * 3, m_train)
+    Y_train -- training labels represented by a numpy array (vector) of shape (1, m_train)
+    X_test -- test set represented by a numpy array of shape (num_px * num_px * 3, m_test)
+    Y_test -- test labels represented by a numpy array (vector) of shape (1, m_test)
+    num_iterations -- hyperparameter representing the number of iterations to optimize the parameters
+    learning_rate -- hyperparameter representing the learning rate used in the update rule of optimize()
+    print_cost -- Set to true to print the cost every 100 iterations
+    
+    Returns:
+    d -- dictionary containing information about the model.
+    """
+    
+    # initialize parameters with zeros (≈ 1 line of code)
+    w, b = initialize_with_zeros(X_train.shape[0])
+
+    # Gradient descent (≈ 1 line of code)
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost);
+    
+    # Retrieve parameters w and b from dictionary "parameters"
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples (≈ 2 lines of code)
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
+    
+    d = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w,
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iterations": num_iterations}
+    
+    return d
+
+# Loading the data (cat/non-cat)
+train_set_x_orig, train_set_y, test_set_x_orig, test_set_y, classes = load_dataset();
+
+# Reshape the training and test examples
+train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T;
+test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T;
+
+
+# Center and standardize the data set
+# 
+#   -- substract the mean of the whole numpy array from each example, and then divide each 
+# example by the standard deviation of the whole numpy array. 
+# 
+#   -- for picture datasets, it is simpler and more convenient and works almost as well to 
+# just divide every row of the dataset by 255 (the maximum value of a pixel channel).
+
+train_set_x = train_set_x_flatten/255.
+test_set_x = test_set_x_flatten/255.
+
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 2000, learning_rate = 0.005, print_cost = True)
+
+# Plot learning curve (with costs)
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
